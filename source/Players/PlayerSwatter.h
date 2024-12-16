bcca57c7a69ec4e394796bcb8e9f03fde678ac8a
Albert <albert.march@enti.cat>
2024-12-16 23:33:13 +0100

    Swatter

--- source/Players/PlayerSwatter.h
+++ source/Players/PlayerSwatter.h
@@ -5,82 +5,127 @@
 #include "../InputManager.h"
 #include "../Rigidbody.h"
 
+enum PlayerState {
+	Normal,
+	Attack,
+	Stuned
+};
+
 class PlayerSwatter : public Player {
 public:
+    bool isColliding;
+    float stateStartTime = 0.0f;
+    PlayerState currentState = Normal;
 
-	bool isColliding;
-	bool isPaused = false;
-	float pauseStartTime = 0.0f;
+    PlayerSwatter(Vector2 pos)
+        : Player("resources/Swatter.png", pos, Vector2(1500.f, 1500.f)) {
+    }
 
-	PlayerSwatter(Vector2 pos)
-		: Player("resources/Swatter.png", pos, Vector2(1500.f, 1500.f)) {
-	}
+    void Update() override {
+        switch (currentState) {
+        case Normal:
+            if (Input.GetLeftClick()) {
+                currentState = Attack;
+                stateStartTime = TIME.GetElapsedTime();
+            }
+            else {
+                Movement();
+            }
+            break;
 
-	void Update() override {
-		if (Input.GetLeftClick()) {
-			
-		}
-		if (isPaused) {
-			HandlePause();
-		}
-		else {
-			Movement();
-		}
-		Player::Update();
-	}
+        case Attack:
+            if (isColliding) {
+                currentState = Normal;
+            }
+            else {
+                float elapsedTime = TIME.GetElapsedTime() - stateStartTime;
+                if (elapsedTime >= 0.2f) {
+                    currentState = Stuned;
+                    stateStartTime = TIME.GetElapsedTime();
+                }
+            }
+            break;
 
-	void HandlePause() {
-		float currentTime = TIME.GetElapsedTime();
-		if (currentTime - pauseStartTime >= 2.0f) {
-			isPaused = false;
-		}
-	}
+        case Stuned:
+            if (TIME.GetElapsedTime() - stateStartTime >= 2.0f) {
+                currentState = Normal;
+            }
+            break;
+        }
 
-	void StartPause() {
-		isPaused = true;
-		pauseStartTime = TIME.GetElapsedTime();
-	}
+        Player::Update();
+    }
 
-	void Movement() override {
-		float mouseX = Input.GetMouseX();
-		float mouseY = Input.GetMouseY();
-		Vector2 mousePosition = Vector2(mouseX, mouseY);
+    void Movement() override {
+        float mouseX = Input.GetMouseX();
+        float mouseY = Input.GetMouseY();
+        Vector2 mousePosition = Vector2(mouseX, mouseY);
 
-		Vector2 direction = mousePosition - transform->position;
+        Vector2 direction = mousePosition - transform->position;
 
-		float distanceSquared = direction.x * direction.x + direction.y * direction.y;
+        float distanceSquared = direction.x * direction.x + direction.y * direction.y;
 
-		if (distanceSquared < 1.0f) {
-			return;
-		}
+        if (distanceSquared < 1.0f) {
+            return;
+        }
 
-		direction.Normalize();
+        direction.Normalize();
 
-		float maxSpeed = 1000.0f;
-		float deltaTime = TIME.GetDeltaTime();
-		Vector2 velocity = direction * maxSpeed * deltaTime;
+        float maxSpeed = 1000.0f;
+        float deltaTime = TIME.GetDeltaTime();
+        Vector2 velocity = direction * maxSpeed * deltaTime;
 
-		if (velocity.x * velocity.x + velocity.y * velocity.y > distanceSquared) {
-			transform->position = mousePosition;
-		}
-		else {
-			transform->position = transform->position + velocity;
-		}
+        if (velocity.x * velocity.x + velocity.y * velocity.y > distanceSquared) {
+            transform->position = mousePosition;
+        }
+        else {
+            transform->position = transform->position + velocity;
+        }
+    }
 
-	}
+    void OnCollisionEnter(Object* other) override {
+        if (Enemy* enemy = dynamic_cast<Enemy*>(other)) {
+            isColliding = true;
+            if (currentState == Attack) {
+                enemy->Destroy();
+                currentState = Normal;
+            }
+        }
+        else {
+            isColliding = false;
+        }
+        
+        if (Enemy* enemy = dynamic_cast<Enemy*>(other)) {
+            float currentTime = TIME.GetElapsedTime();
 
-	void OnCollisionEnter(Object* other) override {
-		if (Input.GetLeftClick()) {
-			
-			if (Enemy* to = reinterpret_cast<Enemy*>(other)) {
-				//Destroy();
-				other->Destroy();
-				StartPause();
-			}
-		}
-	}
+            //2 segons de cooldown
+            if (currentTime - lastDamageTime >= 2.0f) {
 
-	void Render() override {
-		Player::Render();
-	}
+                lives--;
+                lastDamageTime = currentTime;
+
+            }
+        }
+        if (Bullet* bullet = dynamic_cast<Bullet*>(other)) {
+            if (!bullet->IsFriendly()) {
+                float currentTime = TIME.GetElapsedTime();
+
+                if (currentTime - lastDamageTime >= 2.0f) {
+
+                    lives--;
+                    lastDamageTime = currentTime;
+
+                }
+            }
+        }
+        if (lives <= 0) {
+            Destroy();
+            SM.SetNextScene("Splat");
+            lives = 3; //Torna a comensar
+        }
+    }
+
+    void Render() override {
+        Player::Render();
+    }
 };
